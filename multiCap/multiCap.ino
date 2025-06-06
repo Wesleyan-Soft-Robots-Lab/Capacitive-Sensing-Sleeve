f8fd175218cdabd9dd2c31863febf6dfb888724c
MilesM326 <mmodeste@wesleyan.edu>
2025-06-06 17:15:46 -0400

    Unsuccessfully added a calibration function to Sensor class
    +refined debug so that variables show up in the serial plotter

--- multiCap/multiCap.ino
+++ multiCap/multiCap.ino
@@ -11,41 +11,50 @@ Description:
 #include <Protocentral_FDC1004.h>
 #include <List.hpp>
 
-#define TCAADDR1 0x70 // 1st link multiplexer
-#define TCAADDR2 0x71 // 2nd link multiplexer (currently n/a)
+#define TCAADDR1 0x70  // 1st link multiplexer
+#define TCAADDR2 0x71  // 2nd link multiplexer (currently n/a)
 
-#define UPPER_BOUND  0X4000                 // max readout capacitance
-#define LOWER_BOUND  (-1 * UPPER_BOUND)
-#define ONEA 0 // channel
+#define UPPER_BOUND 0x7FFF
+#define LOWER_BOUND (-1 * UPPER_BOUND)
+#define ONEA 0  // channel
 #define ONEB 1
 #define TWOA 2
 #define TWOB 3
 
-#define capdac 5 // Capacitance Digital-to-Analog Converter (subtracts baseline 0-15pF)
-#define SENSOR_COUNT 3
+#define MAX_WINDOW 10  //set 1 to disable; max: 255
+#define SENSOR_COUNT 1
 FDC1004 FDC;
+
 /* Defines Sensor obj: 
-includes addr, channel, window, window_sum, capacitance, and software_offset; 
+includes addr, channel, window, window_sum, capacitance, and ; 
 Sensor() sets defaults (i.e. address & channel = 0)*/
 class Sensor {
-  public:
-    uint8_t bus; //which multiplexor port (0-7) fdc chip is connected to
-    uint8_t channel; //fdc channel
-    List<int32_t> window;
-    int32_t window_sum = 0;
-    int16_t capVal;
-    int16_t software_offset = 0; // in femtoFarads (1000 fF = 1 pF)
+public:
+  uint8_t bus;      //multiplexor port (0-7) fdc chip is connected to
+  uint8_t channel;  //chip channel
 
-  Sensor(uint8_t addr, uint8_t ch) : bus(addr), channel(ch){}
-  Sensor() : bus(0), channel(0) {}
-  // Specifies which mux and port to read from
-  void SetBus(uint8_t bus) 
-  {
+  uint8_t capdac = 0;  // Capacitance Digital-to-Analog Converter (subtracts baseline 0-15pF). Used for calibrating max capVal
+  List<int16_t> window;
+  uint8_t window_len = 0;
+  int32_t window_sum = 0;
+
+  uint16_t capVal;  //avg raw units
+
+  bool isCalibrated = false;
+
+  Sensor(uint8_t addr, uint8_t ch)
+    : bus(addr), channel(ch) {}
+  Sensor()
+    : bus(0), channel(0) {}
+
+  // Switches mux port to read from
+  void SetBus(uint8_t bus) {
     Wire.beginTransmission(TCAADDR1);
     Wire.write(1 << bus);
     Wire.endTransmission();
   }
-  void ReadChannel() {
+
+  void UpdateSensor() {
     SetBus(bus);
     FDC.configureMeasurementSingle(channel, channel, capdac);
     FDC.triggerSingleMeasurement(channel, FDC1004_100HZ);
@@ -54,42 +63,108 @@ class Sensor {
     delay(15);
     uint16_t value[2];
 
-    if (! FDC.readMeasurement(channel, value))
-    {
-      int16_t msb = value[0];
-      int32_t capacitance = ((int32_t)457) * ((int32_t)msb); //in attofarads
-      capacitance /= 1000;   //in femtofarads
-      capacitance += ((int32_t)3028) * ((int32_t)capdac);
+    if (!FDC.readMeasurement(channel, value)) {
+      uint16_t msb = value[0];
 
-      capVal = (uint16_t)capacitance;
+      if (window_len < MAX_WINDOW) {
+        window_sum += msb;
+        window.add(msb);
+        window_len++;
+      }
+      else {
+        window_sum -= window[0];
+        window.removeFirst();
+        window_sum += msb;
+        window.add(msb);
+      }
+      capVal = window_sum/window_len;
+
+      /* int32_t capacitance = ((int32_t)457) * ((int32_t)msb);  //in attofarads
+      capacitance /= 1000;                                    //in femtofarads
+      capacitance += ((int32_t)3028) * ((int32_t)capdac);
+      */
+    }
+  }
+  // Sets appropriate capdac
+  long period = 200;
+
+  void Calibrate() {
+    // set lowAvg
+    uint16_t lo,hi;
+    isCalibrated = false;
+    capdac = 0;
+    //reset window
+    window.clear();
+    window_len = 0;
+    window_sum = 0;
+    unsigned long start = millis();
+    while(true)
+    {
+      UpdateSensor();
+      unsigned long timeDelta = start - millis();
+      if (timeDelta >= period){
+        lo = capVal;
+        capdac ++;
+        break;
+      }
+    }
+    //set hiAvg
+    while (!isCalibrated) {
+      //reset window
+      window.clear();
+      window_len = 0;
+      window_sum = 0;
+      unsigned long start = millis();
+      while (true)
+      {
+        UpdateSensor();
+        unsigned long timeDelta = start - millis();
+        if (timeDelta >= period)
+        {
+          hi = capVal;
+          if (abs(lo) > abs(hi)) 
+          {
+            lo = hi;
+            capdac++;
+          }
+          else if (abs(lo) <= abs(hi))
+          {
+            isCalibrated = true;
+            return;
+          }
+        }
+      }
     }
   }
 };
 
-//capdac and hardware_offset are essentially the same thing (not entirely sure how they affect offset)
-uint8_t hardware_offset = 9; // offset (in pF) = hardware_offset * 3.125 (14)
-
-bool is_calibrated = false;
-int MAX_WINDOW = 10; // max window size for the rolling window stability. Set to 1 to disable.
 Sensor sensors[SENSOR_COUNT];
 
-void initSensors(){
-  sensors[0] = Sensor(0, TWOB);
+void initSensors() {
+  //**Note** adjust SENSORCOUNT accordingly
+  /* sensors[0] = Sensor(0, TWOB);
   sensors[1] = Sensor(7, ONEB);
   sensors[2] = Sensor(0, ONEB);
-
+  sensors[3] = Sensor(0, TWOA); */
+  sensors[0] = Sensor(7, ONEA);
+  for (int i=0; i<SENSOR_COUNT; i++){
+    sensors[i].Calibrate();
+  }
   return;
 }
 
-void Debug()
-{
+void Debug() {
   //Print statements
   String msg = "";
-  for (int i=0; i<SENSOR_COUNT; i++)
-  {
-    int reading = sensors[i].capVal/100;
-    msg = " | sensor_" + (String) i + ": " + (String) reading;
-    Serial.print(msg);
+  for (int i = 0; i < SENSOR_COUNT; i++) {
+    if (i != 0) {
+      msg = ", ";
+    }
+    int reading = sensors[i].capVal;
+    int offset = sensors[i].capdac;
+    //msg += "sensor_" + (String)i + ":" + (String)reading;
+    Serial.print("sensor_" + (String)i + ":");
+    Serial.print(reading);
   }
   Serial.println();
 }
@@ -108,9 +183,8 @@ void setup() {
 
 void loop() {
   //1st Multiplexer
-  for (int i=0; i<SENSOR_COUNT; i++)
-  {
-    sensors[i].ReadChannel();
+  for (int i = 0; i < SENSOR_COUNT; i++) {
+    sensors[i].UpdateSensor();
   }
   Debug();
 }
